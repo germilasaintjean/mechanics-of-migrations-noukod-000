751c0a7a2118ad499486b4ec31f7867f167be4c1
Saint-Jean Germila <germila@noukod.com>
2018-12-04 19:00:58 +0000

    Automatically backed up by Learn

---
+++ artist.rb
@@ -0,0 +1,18 @@
+class Artist
+
+  attr_accessor :name, :genre, :age, :hometown
+
+def initialize(name, genre, age, hometown)
+  name = @name
+  genre = @genre
+  age = @age
+  hometown = @hometown
+end
+def create
+
+end
+
+def column_names
+
+end
+end
